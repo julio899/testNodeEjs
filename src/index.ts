d6bda452b934dc7e7c2aef7f7fc56c61e8eec3b3
Julio Vinachi <julio899@gmail.com>
2019-07-24 19:31:15 -0400

    add loader

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,11 @@ import pathF from "path";
 const app = express();
 
 // Settings and Config
-app.set("port", 3000);
-app.set("views", pathF.join(__dirname, "").replace("build", "src/views/") );
-app.engine('html',require('ejs').renderFile );
-app.set("view engine", 'ejs');
-//console.log(path.join(__dirname, "public").replace("build/", "src/"));
+app.set("port", process.env.PORT || 3000);
+app.set("views", pathF.join(__dirname, "").replace("build", "src/views/"));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "ejs");
+
 // Public
 app.use(
 	express.static(pathF.join(__dirname, "public").replace("build/", "src/"))
@@ -20,4 +20,5 @@ app.use(routes);
 
 app.listen(app.get("port"), function() {
 	console.log("Example app listening on port " + app.get("port"));
+	console.log("views dir: " + app.get("views"));
 });
--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,13 @@
-import express from 'express';
+import express from "express";
 const routes = express();
 import path from "path";
 
 const urlBaseViews = path.join(__dirname, "").replace("build", "src/views/");
-console.log('* url views: '+ urlBaseViews);
+console.log("* url views: " + urlBaseViews);
 
 // Routes
-routes.get('/',(req:any,res:any)=>{
-	// res.sendFile( urlBaseViews + 'home.html');
-	res.render(urlBaseViews + 'home.html',{title:'any case'});
+routes.get("/", (req: any, res: any) => {
+	res.render(urlBaseViews + "home.html", { title: "any case" });
 });
 
 export default routes;
